ea7d3180fa7f7dd9d4a49716f50a00a8fc586649
Gabor Szabo <gabor@szabgab.com>
2024-07-07 18:42:01 +0300

    autocommit

--- test_app.py
+++ test_app.py
@@ -1,8 +1,14 @@
 import app
 
-def test_qqrq():
+def test_app():
     web = app.app.test_client()
 
     rv = web.get("/")
+    assert rv.status_code == 200
+
+def test_missing_page():
+    web = app.app.test_client()
+
+    rv = web.get("/hello")
     assert rv.status_code == 200
 
