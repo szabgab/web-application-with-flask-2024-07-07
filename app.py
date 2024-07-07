e30dfc3108c251ccc64c591c0308503c1fc6882e
Gabor Szabo <gabor@szabgab.com>
2024-07-07 18:52:01 +0300

    autocommit

--- app.py
+++ app.py
@@ -5,9 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return 'Hello <b>World!</b> <a href="/echo">time</a> <a href="/echo">echo</a>'
+    return 'Hello <b>World!</b> <a href="/time">time</a> <a href="/echo">echo</a>'
 
 
 @app.route("/time")
 def show_time():
     return str(time.time())
+
+
+@app.route("/echo")
+def echo():
+    return """
+<form>
+<input name="text">
+</form>
+"""
