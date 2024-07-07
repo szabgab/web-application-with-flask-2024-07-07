744cdbc16742903e894273420b6f0197ebc134fa
Gabor Szabo <gabor@szabgab.com>
2024-07-07 18:46:01 +0300

    autocommit

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return "Hello <b>World!</b>"
+
+
+@app.route("/time")
+def show_time():
+    return time.time()
